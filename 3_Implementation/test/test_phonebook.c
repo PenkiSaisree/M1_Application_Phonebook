14a94ed359fea2e9b6a15cd2011dcc6dd390f126
PenkiSaisree <94159732+PenkiSaisree@users.noreply.github.com>
2021-11-25 21:47:53 +0530

    Update test_phonebook.c

--- 3_Implementation/test/test_phonebook.c
+++ 3_Implementation/test/test_phonebook.c
@@ -3,18 +3,6 @@
 #include<string.h>
 #include<stdlib.h>
 #include<windows.h>
-struct person
-{
-    char name[35];
-    char address[50];
-     char father_name[35];
-     char mother_name[30];
-    long int mble_no;
-    char sex[8];
-    char mail[100];
-    char citision_no[20];
-
-    };
 //All function declaration
 void menu();
 void got();

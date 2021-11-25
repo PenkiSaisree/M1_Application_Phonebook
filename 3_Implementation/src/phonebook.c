e2be7eb8d5a30915d0b2c85954c2f9bd86621ba9
PenkiSaisree <94159732+PenkiSaisree@users.noreply.github.com>
2021-11-25 22:27:31 +0530

    Update phonebook.c

--- 3_Implementation/src/phonebook.c
+++ 3_Implementation/src/phonebook.c
@@ -1,32 +1,39 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<windows.h>
-struct person
+ void addrecord()
 {
-    char name[35];
-    char address[50];
-     char father_name[35];
-     char mother_name[30];
-    long int mble_no;
-    char sex[8];
-    char mail[100];
-    char citision_no[20];
+        system("cls");
+        FILE *f;
+        struct person p;
+        //used to open a file to perform various operations 
+        f=fopen("project","ab+");
+        printf("\n Enter name: ");
+        //Declaring library function 
+        got(p.name);
+        printf("\nEnter the address: ");
+        got(p.address);
+        printf("\nEnter father name: ");
+        got(p.father_name);
+        printf("\nEnter mother name: ");
+        got(p.mother_name);
+        printf("\nEnter phone no.:");
+        scanf("%ld",&p.mble_no);
+        printf("Enter sex:");
+        got(p.sex);
+        printf("\nEnter e-mail:");
+         got(p.mail);
+        printf("\nEnter citizen no:");
+        got(p.citision_no);
+        //function writes the data specified by the void pointer 
+        fwrite(&p,sizeof(p),1,f);
+      
+      //used for output stream to buffer
+      fflush(stdin);
+        printf("\nrecord saved");
+//deletes all buffers that are associated with the stream before closing it
+fclose(f);
 
-    };
-void menu();
-void got();
-void start();
-void back();
-void addrecord();
-void listrecord();
-void modifyrecord();
-void deleterecord();
-void searchrecord();
-int main()
-{
-    system("color 0f");
-    start();
-    return 0;
+    printf("\n\nEnter any key");
+
+	 getch();
+    system("cls");
+    menu();
 }

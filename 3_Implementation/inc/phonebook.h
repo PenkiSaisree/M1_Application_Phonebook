dbf2a0684ea8554b90640e605c40852771115fcf
PenkiSaisree <94159732+PenkiSaisree@users.noreply.github.com>
2021-11-25 19:28:37 +0530

    Update phonebook.h

--- 3_Implementation/inc/phonebook.h
+++ 3_Implementation/inc/phonebook.h
@@ -7,23 +7,6 @@
 #ifndef __HEADER_H__
 #define __HEADER_H__
 
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<windows.h>
-struct person
-{
-    char name[35];
-    char address[50];
-     char father_name[35];
-     char mother_name[30];
-    long int mble_no;
-    char sex[8];
-    char mail[100];
-    char citision_no[20];
-
-    };
 //All function declaration
 void menu();
 void got();
@@ -35,4 +18,4 @@ void modifyrecord();
 void deleterecord();
 void searchrecord();
 
-#endif
+#endif // phonebook_H

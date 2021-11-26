1290c1c24a2babdf987b220e2eaf61600c60d094
PenkiSaisree <94159732+PenkiSaisree@users.noreply.github.com>
2021-11-26 12:33:20 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -9,43 +9,29 @@ int main()
     start();
     return 0;
 }
-      //This function used for adding individual or multiple records
-        void addrecord()
+void listrecord()
 {
-        system("cls");
-        FILE *f;
-        struct person p;
-        //used to open a file to perform various operations 
-        f=fopen("project","ab+");
-        printf("\n Enter name: ");
-        //Declaring library function 
-        got(p.name);
-        printf("\nEnter the address: ");
-        got(p.address);
-        printf("\nEnter father name: ");
-        got(p.father_name);
-        printf("\nEnter mother name: ");
-        got(p.mother_name);
-        printf("\nEnter phone no.:");
-        scanf("%ld",&p.mble_no);
-        printf("Enter sex:");
-        got(p.sex);
-        printf("\nEnter e-mail:");
-         got(p.mail);
-        printf("\nEnter citizen no:");
-        got(p.citision_no);
-        //function writes the data specified by the void pointer 
-        fwrite(&p,sizeof(p),1,f);
-      
-      //used for output stream to buffer
-      fflush(stdin);
-        printf("\nrecord saved");
-//deletes all buffers that are associated with the stream before closing it
-fclose(f);
+    struct person p;
+    FILE *f;
+f=fopen("project","rb");
+if(f==NULL)
+{
+printf("\nfile opening error in listing :");
 
-    printf("\n\nEnter any key");
+exit(1);
+}
+//Indicates the no.of bytes to the each element
+while(fread(&p,sizeof(p),1,f)==1)
+{
+     printf("\n\n\n YOUR RECORD IS\n\n ");
+        printf("\nName=%s\nAdress=%s\nFather name=%s\nMother name=%s\nMobile no=%ld\nSex=%s\nE-mail=%s\nCitizen no=%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
 
 	 getch();
+	 system("cls");
+}
+fclose(f);
+ printf("\n Enter any key");
+ getch();
     system("cls");
-    menu();
+menu();
 }
